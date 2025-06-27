893b35908d307ab8cff590e5edd7d8828dd793cf
Greg Griffin <greg.griffin2@gmail.com>
2025-06-26 20:33:06 -0400

    add command boilerplate

---
+++ cmd/run.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2025 Greg Griffin <greg.griffin2@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run [model to prompt]",
+	Short: "Create a new prompt session with a model",
+	Long:  `Begin a prompt session with a specified model, creating a new server for convenience. Server will be shutdown on exit`,
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("run called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/serve.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2025 Greg Griffin <greg.griffin2@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve [options]",
+	Short: "Start a server that handles any number of prompt sessions",
+	Long:  `Starts a local server to handle prompt sessions. To begin a prompt session, run the 'llm prompt' command in a seperate shell`,
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
