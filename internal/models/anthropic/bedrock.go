f3e119f8413a4b3a1ed3038494852b70b4ba21b3
Greg Griffin <greg.griffin2@gmail.com>
2026-04-04 15:35:19 -0400

    use aws sdk for adding extra certs

--- internal/models/anthropic/bedrock.go
+++ internal/models/anthropic/bedrock.go
@@ -2,15 +2,11 @@ package anthropic
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/anthropics/anthropic-sdk-go/bedrock"
 	"github.com/anthropics/anthropic-sdk-go/option"
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/gregriff/ducky/internal/models"
 )
@@ -21,20 +17,16 @@ func buildBedrockConfig(
 	bedrockConfig *models.BedrockConfig,
 	opts []option.RequestOption,
 ) ([]option.RequestOption, error) {
-	if !bedrockConfig.ExtraCerts {
-		cfg, err := config.LoadDefaultConfig(ctx)
+	var certsPEM *os.File
+	var err error
+	if bedrockConfig.ExtraCerts && bedrockConfig.CertsPath != "" {
+		certsPEM, err = os.Open(bedrockConfig.CertsPath)
 		if err != nil {
-			return nil, fmt.Errorf("error creating aws config: %w", err)
+			return nil, fmt.Errorf("error opening extra ca certs file: %w", err)
 		}
-		return append(opts, bedrock.WithConfig(cfg)), nil
 	}
 
-	httpClient, err := newCustomCertClient(bedrockConfig.CertsPath)
-	if err != nil {
-		return nil, fmt.Errorf("error creating httpClient with extra certs: %w", err)
-	}
-
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithHTTPClient(httpClient))
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithCustomCABundle(certsPEM))
 	if err != nil {
 		return nil, fmt.Errorf("error loading default aws config with extra certs: %w", err)
 	}
@@ -49,30 +41,4 @@ func buildBedrockConfig(
 	cfg.BearerAuthTokenProvider = nil
 
 	return append(opts, bedrock.WithConfig(cfg)), nil
-}
-
-func newCustomCertClient(certPath string) (*awshttp.BuildableClient, error) {
-	if certPath == "" {
-		return nil, nil
-	}
-
-	certPEM, err := os.ReadFile(certPath)
-	if err != nil {
-		return nil, fmt.Errorf("error reading CA cert: %w", err)
-	}
-
-	rootCAs, err := x509.SystemCertPool()
-	if err != nil {
-		rootCAs = x509.NewCertPool() // fallback if system pool unavailable
-	}
-
-	if ok := rootCAs.AppendCertsFromPEM(certPEM); !ok {
-		return nil, fmt.Errorf("error appending CA cert")
-	}
-
-	httpClient := awshttp.NewBuildableClient().WithTransportOptions(func(tr *http.Transport) {
-		tr.TLSClientConfig = &tls.Config{RootCAs: rootCAs}
-	})
-
-	return httpClient, nil
 }
